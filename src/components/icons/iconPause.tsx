b185e78536a7302416eaaff529fe47ee54e9e458
marynag <mgrabchak1109@gmail.com>
2023-05-12 10:56:12 +0300

    implement svg icons

---
+++ src/components/icons/iconPause.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const IconPause = () => {
+  return ( 
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path d="M10 24h-6v-24h6v24zm10-24h-6v24h6v-24z" fill="#0C1831"/>
+    </svg>    
+  )
+}
+
+export default IconPause
---
+++ src/components/icons/iconPlay.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const IconPlay = () => {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" >
+        <title>icon/16/play</title>
+        <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="icon/16/play">
+                <path d="M9.16357066,2.32714132 L14.9313296,13.8626591 C15.0685458,14.1370916 14.9573101,14.4707989 14.6828776,14.6080151 C14.6057357,14.6465861 14.5206728,14.6666667 14.4344256,14.6666667 L2.89890777,14.6666667 C2.59208291,14.6666667 2.34335222,14.417936 2.34335222,14.1111111 C2.34335222,14.0248638 2.36343282,13.939801 2.40200378,13.8626591 L8.16976267,2.32714132 C8.30697892,2.05270882 8.64068617,1.94147308 8.91511866,2.07868933 C9.02263389,2.13244694 9.10981305,2.2196261 9.16357066,2.32714132 Z" id="Triangle" fill="#0C1831" transform="translate(8.666667, 8.000000) rotate(90.000000) translate(-8.666667, -8.000000) "></path>
+                <rect id="Rectangle" x="0" y="0" width="16" height="16"></rect>
+            </g>
+        </g>
+    </svg>
+  )
+}
+
+export default IconPlay
