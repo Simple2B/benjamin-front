ca9093a4744a164983e79dd2589a1dacefd4665a
marynag <mgrabchak1109@gmail.com>
2023-05-16 18:02:49 +0300

    soldier additional image

---
+++ src/components/SoldierAdditionalImage.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type ISoldierAdditionalImageProps = {
+    imageUrl:string,
+    imageDescription: string
+}
+
+const SoldierAdditionalImage = ({imageUrl, imageDescription}: ISoldierAdditionalImageProps) => {
+  return (
+    <>
+      {imageUrl && (
+        <div className='mt-3'>
+          <p className='text-sm text-grey-20'>{imageDescription}</p>
+          <img className='w-full h-56 rounded-lg bg-grey-30' src={imageUrl} alt="document" />
+        </div>
+      )}
+    </>
+  );  
+};
+
+export default SoldierAdditionalImage;
