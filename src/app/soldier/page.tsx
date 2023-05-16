c8876923603f08a9e7c9439c70727b272ad26424
marynag <mgrabchak1109@gmail.com>
2023-05-16 13:10:46 +0300

    soldier photo and main info

--- src/app/soldier/page.tsx
+++ src/app/soldier/page.tsx
@@ -1,8 +1,11 @@
+import SoldierMainInfoCard from '@/components/SoldierMainInfoCard';
 import React from 'react';
 
 export default function Page() {
   return (
-    <div>soldier</div>
+    <div className='flex flex-col justify-center items-center'>
+      <SoldierMainInfoCard photoUrL='#' name='1st Lt. Robert Fink' serviceNumber='O-430368' branchOfService='US Coast Guard' awards='Purple Heart' />
+    </div>
   );
 }
 
