e118f2e955aa69f84a651b8fbb0a10f1ffa52f5e
marynag <mgrabchak1109@gmail.com>
2023-05-17 15:19:56 +0300

    update search bar styles and passing state

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,15 +3,20 @@ import React, { useState } from 'react';
 import IconButton from './IconButton';
 import { ICONS_NAME } from './constants/iconName';
 
-const SearchBar = () => {
+type ISearchBarProps = {
+  setState: (value: string) => void;
+}
+
+const SearchBar = ({setState}: ISearchBarProps) => {
   const [userInput, setUserInput] = useState<string>();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
     setUserInput(e.target.value);
+    setState(e.target.value);
   };
 
   return (
-    <div className='inline-flex items-center gap-2 rounded-lg bg-white h-12 justify-evenly px-1.5 w-80'>
+    <div className='relative inline-flex items-center gap-2 rounded-lg bg-white h-12 justify-evenly px-1.5 w-80 mt-10'>
       <IconButton iconName={ICONS_NAME.magnifyingGlass} className={'h-6 w-6'}/> 
       <input type='text' placeholder='Seach for the soldier' onChange={handleChange} value={userInput} />
       <IconButton iconName={ICONS_NAME.camera} className={'h-6 w-6'}/> 		
