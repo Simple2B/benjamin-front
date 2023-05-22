a7fea5b5430be6482cb5b6d62c9517c81321e292
marynag <mgrabchak1109@gmail.com>
2023-05-22 09:41:29 +0300

    add listener to video end

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 type IVideoPlayeProps = {
   srcVideo: string;
+  onVideoEnd: () => void;
 };
 
-const VideoPlayer = ({ srcVideo }: IVideoPlayeProps) => {
+const VideoPlayer = ({ srcVideo, onVideoEnd }: IVideoPlayeProps) => {
+  const videoRef: HTMLVideoElement = useRef();
+
+  useEffect(() => {
+    if (onVideoEnd) {
+      const timerId = setInterval(() => {
+        if (videoRef.current.ended) {
+          onVideoEnd();
+          clearInterval(timerId);
+        }
+      }, 100);
+    }
+  }, []);
+
   return (
     <div>
-      <video autoPlay muted>
+      <video autoPlay muted ref={videoRef}>
         <source src={srcVideo} type="video/mp4" />
       </video>
     </div>
--- src/components/projectInfo/ProjectInfo.tsx
+++ src/components/projectInfo/ProjectInfo.tsx
@@ -6,12 +6,18 @@ type IProjectInfoProps = {
   heading: string;
   text: string;
   vireoUrl: string;
+  onVideoEnd: () => void;
 };
 
-const ProjectInfo = ({ heading, text, vireoUrl }: IProjectInfoProps) => {
+const ProjectInfo = ({
+  heading,
+  text,
+  vireoUrl,
+  onVideoEnd,
+}: IProjectInfoProps) => {
   return (
     <>
-      <VideoPlayer srcVideo={vireoUrl} />
+      <VideoPlayer srcVideo={vireoUrl} onVideoEnd={onVideoEnd} />
       <div className="h-72">
         <div className="flex flex-col p-6 gap-2 text-indigo-100">
           <h1 className="text-2xl font-extrabold leading-none tracking-tight">
