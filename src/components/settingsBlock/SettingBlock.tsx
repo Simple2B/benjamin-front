45948a8a7c66250f3d9d901467c816f164827296
marynag <mgrabchak1109@gmail.com>
2023-05-22 16:46:25 +0300

    add link to each settingsBox

--- src/components/settingsBlock/SettingBlock.tsx
+++ src/components/settingsBlock/SettingBlock.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { SETTINHS_INFO } from './SettingBlock.constants';
 import SettingsBox from '../SettingsBox';
+import Link from 'next/link';
 
 const SettingBlock = () => {
   return (
     <div className="flex flex-col gap-4 w-full">
-      {SETTINHS_INFO.map(({ iconName, boxName }) => (
-        <SettingsBox iconName={iconName} boxName={boxName} key={boxName} />
+      {SETTINHS_INFO.map(({ iconName, boxName, link }) => (
+        <Link href={link} key={boxName}>
+          <SettingsBox iconName={iconName} boxName={boxName} />
+        </Link>
       ))}
     </div>
   );
