a197726048744e6dfe7f247154b9d2654ee488ef
marynag <mgrabchak1109@gmail.com>
2023-06-02 17:48:12 +0300

    add: connect audio tour with backend

--- src/components/cemetery/PreviewCementery.tsx
+++ src/components/cemetery/PreviewCementery.tsx
@@ -2,15 +2,15 @@
 import React, { useState } from 'react';
 import { CemeteryOut } from '@/openapi';
 import CemeteryAdditionalInfo from './cemeteryAdditionalInfo/CemeteryAdditionalInfo';
-
 import HorizontalPhotoGallery from '../HorizontalPhotoGallery';
 import MapCemetery from './MapCemetery';
 import SearchBar from '../SearchBar';
 import SelectingCemetery from '../SelectingCementery';
-import { ICONS_NAME } from '../constants/iconName';
-import CemeteryMainInfo, {
-  IContactInfo,
-} from './cemeteryMainInfo/CemeteryMainInfo';
+import CemeteryMainInfo from './cemeteryMainInfo/CemeteryMainInfo';
+import { useRouter } from 'next/navigation';
+import urlJoin from 'url-join';
+import { PATH } from '../constants/path.constants';
+import { CemeteryAudioBox } from './cemeteryMainInfo/CemeteryAudioBox';
 
 interface CemeteryPageProps {
   cemetery: CemeteryOut;
@@ -22,26 +22,12 @@ export default function PreviewCementery({
   cemeteries,
 }: CemeteryPageProps) {
   const [inputSoldier, setInputSoldier] = useState<string>('');
-  const [selectedCemetery, setSelectedCemetery] =
-    useState<CemeteryOut>(cemetery);
+  const router = useRouter();
 
-  const contactInfo: IContactInfo[] = [
-    {
-      icon: ICONS_NAME.telephone,
-      description: 'Call',
-      link: cemetery.phone,
-    },
-    {
-      icon: ICONS_NAME.envelope,
-      description: 'Email',
-      link: cemetery.email,
-    },
-    {
-      icon: ICONS_NAME.web,
-      description: 'Website',
-      link: cemetery.webUrl,
-    },
-  ];
+  const handleSelectCemetery = (cemetery: CemeteryOut) => {
+    router.push(urlJoin(PATH.cemetery, cemetery.uuid));
+  };
+
   return (
     <>
       <div className="flex flex-col gap-5 items-center w-full">
@@ -51,21 +37,22 @@ export default function PreviewCementery({
         </div>
 
         <SelectingCemetery
-          setCemetery={setSelectedCemetery}
-          selectedCemetery={selectedCemetery}
+          selectedCemetery={cemetery}
+          onSelect={handleSelectCemetery}
           cemeteries={cemeteries}
-          isRedirecting={true}
         />
         <CemeteryMainInfo
-          name={selectedCemetery.name}
-          location={selectedCemetery.location ? selectedCemetery.location : ''}
-          contactInfo={contactInfo}
-          audioSrc="https://www.bensound.com/bensound-music/bensound-tenderness.mp3"
+          name={cemetery.name}
+          location={cemetery.location ? cemetery.location : ''}
+          phone={cemetery.phone}
+          email={cemetery.email}
+          webUrl={cemetery.webUrl}
         />
+        <CemeteryAudioBox audio_tours={cemetery.audio_tours} />
         <div className="flex flex-col gap-6 items-center w-full px-6 z-10">
           <CemeteryAdditionalInfo
-            superintendent={selectedCemetery.superintendent}
-            war={selectedCemetery.war}
+            superintendent={cemetery.superintendent}
+            war={cemetery.war}
             numberOfSoldiersBuried={12000}
             numberOfJewishSoldiersBuried={250}
             listedAsMissingSoldiers={500}
---
+++ src/components/cemetery/cemeteryMainInfo/CemeteryAudioBox.tsx
@@ -0,0 +1,25 @@
+import AudioPlayer from '@/components/audioPlayer/AudioPlayer';
+import { AWS_BASE_URL } from '@/components/constants/constants';
+import React from 'react';
+import urlJoin from 'url-join';
+
+type ICemeteryAudioBoxProps = {
+  audio_tours: string[];
+};
+
+export const CemeteryAudioBox = ({ audio_tours }: ICemeteryAudioBoxProps) => {
+  return (
+    <div className="w-full flex flex-col gap-6 bg-white px-6 z-10">
+      <div className="flex flex-col gap-2">
+        <p className="font-semibold text-indigo-100">Audio Tour</p>
+        {audio_tours.map((melody) => (
+          <AudioPlayer
+            key={melody}
+            audioSourse={urlJoin(AWS_BASE_URL, melody)}
+          />
+        ))}
+      </div>
+      <hr />
+    </div>
+  );
+};
