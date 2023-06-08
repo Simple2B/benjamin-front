1b0de6bde9d9cc512e8f2f13881e8fbe8afc3ce5
marynag <mgrabchak1109@gmail.com>
2023-06-08 18:15:51 +0300

    fix: pass soldier info

--- src/components/soldier/PreviewerSoldier.tsx
+++ src/components/soldier/PreviewerSoldier.tsx
@@ -6,93 +6,136 @@ import SoldierAdditionalImage from '@/components/soldier/SoldierAdditionalImage'
 import SoldierCardBlockInfo from '@/components/soldier/SoldierCardBlockInfo';
 import SoldierMainInfoCard from '@/components/soldier/SoldierMainInfoCard';
 import React from 'react';
+import { SoldierCoordinates } from './SoldierCoordinates';
 
 export interface ISoldierInfo {
   criteriaName: string;
-  criteriaValue: string;
+  criteriaValue: string | string[];
 }
+
+const soldierInfo = {
+  name: '1st Lt. Robert Fink',
+  serviceNumber: 'O-430368',
+  branchOfService: 'US Coast Guard',
+  photoUrl:
+    'https://i.pinimg.com/564x/93/71/ef/9371ef654aa84ecb36a098feeb3a080d.jpg',
+  awards: ['Air Medal', 'Purple Heart'],
+  audioSourse:
+    'https://www.bensound.com/bensound-music/bensound-tenderness.mp3',
+  finalBurialLocation: 'Plot D, Row 9, Grave 43',
+  finalBurialCoordinates: [54, 89],
+  birthDate: 'December 15th, 1927',
+  birthLocation: 'Edminton, New Hampshire',
+  stateEnteredServiceFrom: "Fort Jay (Governor's Island), New York",
+  assignment: '583rd Signal Air Warning Battalion "C"',
+  position: 'Gunner',
+  jewishServicemanCard: '#',
+  dateOfDeath: 'June 1, 1944',
+  circumstancesOfDeath:
+    'Succumbed to scrub typhus at the 2nd Station Hospital in Nadzab',
+  initialBurial: 'Nadzab, Papua New Guinea',
+  killedinActionTelegram: '#',
+  ceremonyVideoUrl:
+    'https://www.youtube.com/watch?v=bcNdYOsTcTg&list=RDbcNdYOsTcTg&start_radio=1',
+  messages: [
+    {
+      sender: 'Message from Operation Benjamin ',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus libero elit, vel viverra est dignissim eu. Nam facilisis dapibus lorem vitae euismod. Donec congue velit a tellus cursus posuere. Donec pretium, ipsum ac tristique fringilla, nisl lectus auctor felis, sit amet accumsan nisi urna non nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris tincidunt imperdiet eros id porta. Phasellus semper, libero at commodo dignissim, mi massa rutrum nisi, nec luctus arcu risus sed sem. Praesent faucibus imperdiet nibh, et pellentesque ligula convallis in. Aliquam id pulvinar dolor, in volutpat est. Aenean finibus.',
+    },
+    {
+      sender: 'Message from Robert Fink’s family',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus libero elit, vel viverra est dignissim eu. Nam facilisis dapibus lorem vitae euismod. Donec congue velit a tellus cursus posuere. Donec pretium, ipsum ac tristique fringilla, nisl lectus auctor felis, sit amet accumsan nisi urna non nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris tincidunt imperdiet eros id porta. Phasellus semper, libero at commodo dignissim, mi massa rutrum nisi, nec luctus arcu risus sed sem. Praesent faucibus imperdiet nibh, et pellentesque ligula convallis in. Aliquam id pulvinar dolor, in volutpat est. Aenean finibus.',
+    },
+  ],
+};
 
 export default function PreviewerSoldier() {
   const lifeInfo: ISoldierInfo[] = [
     {
       criteriaName: 'Birth date',
-      criteriaValue: 'December 15th, 1927',
+      criteriaValue: soldierInfo.birthDate,
     },
     {
       criteriaName: 'Birth location',
-      criteriaValue: 'Edminton, New Hampshire',
+      criteriaValue: soldierInfo.birthLocation,
     },
   ];
 
   const serviceInfo: ISoldierInfo[] = [
     {
       criteriaName: 'Service number',
-      criteriaValue: 'O-430368',
+      criteriaValue: soldierInfo.serviceNumber,
     },
     {
       criteriaName: 'State entered service from',
-      criteriaValue: "Fort Jay (Governor's Island), New York",
+      criteriaValue: soldierInfo.stateEnteredServiceFrom,
     },
     {
       criteriaName: 'Branch of service',
-      criteriaValue: 'US Airforce',
+      criteriaValue: soldierInfo.branchOfService,
     },
     {
       criteriaName: 'Assignment',
-      criteriaValue: '583rd Signal Air Warning Battalion "C"',
+      criteriaValue: soldierInfo.assignment,
     },
     {
       criteriaName: 'Position',
-      criteriaValue: 'Gunner',
+      criteriaValue: soldierInfo.position,
     },
     {
       criteriaName: 'Awards',
-      criteriaValue: 'Air Medal, Purple Heart',
+      criteriaValue: soldierInfo.awards,
     },
   ];
 
   const deathInfo: ISoldierInfo[] = [
     {
       criteriaName: 'Date of death',
-      criteriaValue: 'June 1, 1944',
+      criteriaValue: soldierInfo.dateOfDeath,
     },
     {
       criteriaName: 'Circumstances of death',
-      criteriaValue:
-        'Succumbed to scrub typhus at the 2nd Station Hospital in Nadzab',
+      criteriaValue: soldierInfo.circumstancesOfDeath,
     },
     {
       criteriaName: 'Initial burial ',
-      criteriaValue: 'Initial burial ',
+      criteriaValue: soldierInfo.initialBurial,
     },
     {
-      criteriaName: 'Initial burial ',
-      criteriaValue:
-        'Manila American Cemetery (Plot A, Row 7, Grave 79), Taguig City, Philippines',
+      criteriaName: 'Final burial location',
+      criteriaValue: soldierInfo.finalBurialLocation,
     },
   ];
 
   return (
     <div>
-      <div className="flex flex-col justify-center items-center mx-7 gap-4 my-4">
+      <div className="flex flex-col justify-center items-center mx-7 gap-4 my-4 text-indigo-100 leading-7">
         <SoldierMainInfoCard
           photoUrl="/images/photos/soldier2.jpg"
-          name="1st Lt. Robert Fink"
-          serviceNumber="O-430368"
-          branchOfService="US Coast Guard"
-          awards="Purple Heart"
+          name={soldierInfo.name}
+          serviceNumber={soldierInfo.serviceNumber}
+          branchOfService={soldierInfo.branchOfService}
+          awards={soldierInfo.awards}
         />
-        <div className="w-full bg-grey-10 rounded-lg p-3">
-          <p className="text-sm text-grey-20">Audio Tour</p>
-          <AudioPlayer audioSourse="https://www.bensound.com/bensound-music/bensound-tenderness.mp3" />
-        </div>
+        {soldierInfo.audioSourse && (
+          <div className="w-full bg-grey-10 rounded-lg p-3">
+            <p className="text-sm text-grey-20">Audio Tour</p>
+            <AudioPlayer audioSourse={soldierInfo.audioSourse} />
+          </div>
+        )}
+
+        <SoldierCoordinates
+          finalBurialCoordinates={soldierInfo.finalBurialCoordinates}
+          finalBurialLocation={soldierInfo.finalBurialLocation}
+        />
+
         <ClosebleInfo heading="LIFE">
           <SoldierCardBlockInfo solderInfo={lifeInfo} />
         </ClosebleInfo>
         <ClosebleInfo heading="SERVICE">
           <SoldierCardBlockInfo solderInfo={serviceInfo} />
           <SoldierAdditionalImage
-            imageUrl="#"
+            imageUrl={soldierInfo.jewishServicemanCard}
             imageDescription="Jewish Serviceman’s card"
           />
         </ClosebleInfo>
@@ -100,19 +143,21 @@ export default function PreviewerSoldier() {
         <ClosebleInfo heading="DEATH">
           <SoldierCardBlockInfo solderInfo={deathInfo} />
           <SoldierAdditionalImage
-            imageUrl="#"
+            imageUrl={soldierInfo.killedinActionTelegram}
             imageDescription="Killed In Action (KIA) Telegram"
           />
         </ClosebleInfo>
 
-        <ClosebleInfo heading="CHANGE CEREMONY">
-          <SoldierAdditionalVideo
-            videoUrl="-"
-            videoDescription="Replacement ceremony video"
-          />
-        </ClosebleInfo>
+        {soldierInfo.ceremonyVideoUrl && (
+          <ClosebleInfo heading="CHANGE CEREMONY">
+            <SoldierAdditionalVideo
+              videoUrl={soldierInfo.ceremonyVideoUrl}
+              videoDescription="Replacement ceremony video"
+            />
+          </ClosebleInfo>
+        )}
       </div>
-      <RememberSoldier name="1st Lt. Robert Fink" />
+      <RememberSoldier name={soldierInfo.name} />
     </div>
   );
 }
