6beab37d02025c45fa7ffc19765a5719e3908b17
marynag <mgrabchak1109@gmail.com>
2023-06-14 09:38:47 +0300

    fix: routes types error

--- src/app/api/cemetery/[cemeteryUuid]/soldier/route.tsx
+++ src/app/api/cemetery/[cemeteryUuid]/soldier/route.tsx
@@ -13,8 +13,12 @@ interface IContext {
 export async function GET(request: Request, context: IContext) {
   const { searchParams } = new URL(request.url);
   const q = searchParams.get('q') || '';
-  const page = searchParams.get('page');
-  const perPage = searchParams.get('perPage');
+  const page = searchParams.get('page')
+    ? Number(searchParams.get('page'))
+    : undefined;
+  const perPage = searchParams.get('perPage')
+    ? Number(searchParams.get('perPage'))
+    : undefined;
 
   const { params } = context;
   if (!params.cemeteryUuid) {
