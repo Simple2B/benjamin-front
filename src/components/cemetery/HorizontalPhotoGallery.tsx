0d841c2b35bff41d56f35dbed9575926488a972e
marynag <mgrabchak1109@gmail.com>
2023-06-22 14:12:08 +0300

    fix: allow scrolling top at cemetery fullpage view

--- src/components/cemetery/HorizontalPhotoGallery.tsx
+++ src/components/cemetery/HorizontalPhotoGallery.tsx
@@ -14,24 +14,28 @@ const HorizontalPhotoGallery = ({
   className,
 }: IHorizontalPhotoGalleryProps) => {
   return (
-    <div className={`w-full ${className}`}>
-      <p className="text-indigo-100 font-semibold mb-3 leading-6 pl-5">
-        {text}
-      </p>
-      <div className="flex gap-3 overflow-x-auto pb-4  text-indigo-100 px-6">
-        {solders.map(({ soldierTitlePhoto, name, uuid }, index) => (
-          <SoldierProfile
-            photoUrl={soldierTitlePhoto}
-            name={name}
-            uuid={uuid}
-            key={index}
-          />
-        ))}
-      </div>
-      <div className="mx-6">
-        <hr className="border-indigo-100 border-opacity-10" />
-      </div>
-    </div>
+    <>
+      {solders.length ? (
+        <div className={`w-full ${className}`}>
+          <p className="text-indigo-100 font-semibold mb-3 leading-6 pl-5">
+            {text}
+          </p>
+          <div className="flex gap-3 overflow-x-auto pb-4  text-indigo-100 px-6">
+            {solders.map(({ soldierTitlePhoto, name, uuid }, index) => (
+              <SoldierProfile
+                photoUrl={soldierTitlePhoto}
+                name={name}
+                uuid={uuid}
+                key={index}
+              />
+            ))}
+          </div>
+          <div className="mx-6">
+            <hr className="border-indigo-100 border-opacity-10" />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
