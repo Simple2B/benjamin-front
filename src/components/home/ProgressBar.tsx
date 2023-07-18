c31ab242f431fa4d41aac724ea97edd49701e980
marynag <mgrabchak1109@gmail.com>
2023-07-18 18:00:07 +0300

    fix: loading height

--- src/components/home/ProgressBar.tsx
+++ src/components/home/ProgressBar.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type IProgressBarProps = { presentLoaded: number };
 
 export const ProgressBar = ({ presentLoaded }: IProgressBarProps) => {
+  const [videoHeight, setVideoHeight] = React.useState<number>(0);
+
+  useEffect(() => {
+    setVideoHeight(window.screen.width);
+  }, []);
+
+  const videoStyle = {
+    height: videoHeight,
+  };
+
   const progressStyle = {
     width: `${presentLoaded}%`,
   };
 
   return (
-    <div className="w-full bg-gradient-to-r from-indigo-20 to-indigo-30 flex flex-col justify-center items-center ">
+    <div
+      className="w-full bg-gradient-to-r from-indigo-20 to-indigo-30 flex flex-col justify-center items-center "
+      style={videoStyle}
+    >
       <p className="text-white">Loading...</p>
       <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700 p-0.5">
         <div
