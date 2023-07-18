590cf2537b27905d2e0f367960f7a52c63c37574
marynag <mgrabchak1109@gmail.com>
2023-07-18 11:15:45 +0300

    fix: el maleh name to prayer

--- src/components/prayer/PreviewPrayer.tsx
+++ src/components/prayer/PreviewPrayer.tsx
@@ -9,8 +9,26 @@ import { prayers } from './PreviewPrayer.constants';
 
 type IPreviewPrayerProps = { elMaleh: string | undefined };
 
+export const formElMalehText = (elMalehName: string | undefined) => {
+  const firstPartPrayer =
+    'אֵל מָלֵא רַחֲמִים, שׁוֹכֵן בַּמְּרוֹמִים, הַמְצֵא מְנוּחָה נְכוֹנָה עַל כַּנְפֵי הַשְּׁכִינָה, בְּמַעֲלות קְדוֹשִׁים וּטְהוֹרִים כְּזֹהַר הָרָקִיעַ מַזְהִירִים, אֶת נִשְׁמַת';
+  const secondPartPrayer =
+    'שֶׁהָלַךְ לְעוֹלָמוֹ, בַּעֲבוּר שֶׁבְּלִי נֶדֶר אֶתֵּן צְדָקָה בְּעַד הַזְכַּרַת נִשְׁמָתוֹ, בְּגַן עֵדֶן תְּהֵא מְנוּחָתוֹ, לָכֵן בַּעַל הָרַחֲמִים יַסְתִּירֵהוּ בְּסֵתֶר כְּנָפָיו לְעוֹלָמִים, וְיִצְרוֹר בִּצְרוֹר הַחַיִּים אֶת נִשְׁמָתוֹ, יְיָ הוּא נַחֲלָתוֹ, וְיָנוּחַ עַל מִשְׁכָּבוֹ בְּשָׁלוֹם. וְנֹאמַר אָמֵן.';
+  const formatedText = [
+    {
+      prayerInNativeLanguage: `${firstPartPrayer} ${elMalehName} ${secondPartPrayer}`,
+      translation: `God, full of mercy, who dwells on high, grant proper rest upon the wings of the Divine Presence, on the levels of the holy 
+    and pure who shine like the splendor of heaven, for the soul of ${elMalehName} who went to his eternal world, as I will give charity for
+     the memory of his soul; let his rest be in the Garden of Eden. Hence, Master of mercy, shelter him in the cover of His wings forever and 
+     bind his soul with the binding of everlasting life. God is his inheritance and let him rest in peace. And let us say Amen. `,
+    },
+  ];
+  return formatedText;
+};
+
 export const PreviewPrayer = ({ elMaleh }: IPreviewPrayerProps) => {
   const router = useRouter();
+  const elMalehText = formElMalehText(elMaleh);
 
   return (
     <div className="flex flex-col py-6 text-indigo-100 justify-center w-full items-center px-5">
@@ -24,22 +42,25 @@ export const PreviewPrayer = ({ elMaleh }: IPreviewPrayerProps) => {
       </div>
       <div className="w-[350px] mt-8">
         <Tab.Group>
-          <Tab.List className="flex w-full space-x-1 text-indigo-100">
+          <Tab.List className="flex w-full space-x-1 text-indigo-100 gap-3">
             {Object.keys(prayers).map((prayer) => (
               <Tab
                 key={prayer}
                 className={({ selected }) =>
-                  `w-full rounded-lg leading-5
-                 ${selected ? 'font-semibold underline' : 'text-grey-30'}`
+                  `w-full rounded-lg leading-5 bg-transparent outline-none font-semibold
+                 ${selected ? 'underline decoration-4' : 'text-grey-30'} 
+                 ${prayer == 'EL MALEH' ? 'flex-shrink-[0.6]' : ''}`
                 }
               >
-                {prayer === 'EL MALEH'
-                  ? `EL MALEH ${elMaleh?.toUpperCase()}`
-                  : prayer}
+                <div>
+                  {prayer === 'EL MALEH'
+                    ? `EL MALEH ${elMaleh?.toUpperCase()}`
+                    : prayer}
+                </div>
               </Tab>
             ))}
           </Tab.List>
-          <Tab.Panels className="mt-2 w-full">
+          <Tab.Panels className="mt-8 w-full">
             {Object.values(prayers).map((posts, idx) => (
               <Tab.Panel key={idx}>
                 <ul>
@@ -50,7 +71,7 @@ export const PreviewPrayer = ({ elMaleh }: IPreviewPrayerProps) => {
                       </p>
                       {post.prayerSource.length > 0 && (
                         <div key={index} className="flex gap-6">
-                          <p className="font-semibold leading-6 text-xl">
+                          <p className="font-semibold leading-6">
                             {post.prayerSource[0]}
                           </p>
                           <p className="font-bold leading-6 text-xl font-['Arial']">
@@ -61,15 +82,38 @@ export const PreviewPrayer = ({ elMaleh }: IPreviewPrayerProps) => {
                       <div className="photo-shadow p-3 rounded-lg border-gray-100 border">
                         <AudioPlayer audioSourse={post.audioUrl} />
                       </div>
-                      {post.prayerText.map(
-                        ({ prayerInNativeLanguage, translation }, index) => (
-                          <div key={index}>
-                            <p className="font-bold leading-5 font-['Arial']">
-                              {prayerInNativeLanguage}
-                            </p>
-                            <p className="leading-5">{translation}</p>
-                          </div>
-                        )
+                      {post.prayerText.length ? (
+                        <>
+                          {post.prayerText.map(
+                            (
+                              { prayerInNativeLanguage, translation },
+                              index
+                            ) => (
+                              <div key={index}>
+                                <p className="font-bold leading-5 font-['Arial']">
+                                  {prayerInNativeLanguage}
+                                </p>
+                                <p className="leading-5">{translation}</p>
+                              </div>
+                            )
+                          )}
+                        </>
+                      ) : (
+                        <>
+                          {elMalehText.map(
+                            (
+                              { prayerInNativeLanguage, translation },
+                              index
+                            ) => (
+                              <div key={index}>
+                                <p className="font-bold leading-5 font-['Arial'] mb-6">
+                                  {prayerInNativeLanguage}
+                                </p>
+                                <p className="leading-5">{translation}</p>
+                              </div>
+                            )
+                          )}
+                        </>
                       )}
                     </div>
                   ))}
