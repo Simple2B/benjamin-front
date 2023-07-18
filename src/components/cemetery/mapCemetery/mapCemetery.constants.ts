12aeff5604b919acbc49698e292e19fdcacaca10
marynag <mgrabchak1109@gmail.com>
2023-07-18 20:27:58 +0300

    fix: location pin David's star

--- src/components/cemetery/mapCemetery/mapCemetery.constants.ts
+++ src/components/cemetery/mapCemetery/mapCemetery.constants.ts
@@ -3,9 +3,9 @@ import L from 'leaflet';
 export const davidStarIcon = L.icon({
   iconUrl: '/images/icons/location-pin-davig-star.svg',
   shadowUrl: '',
-  iconSize: [20, 65],
+  iconSize: [70, 135],
   shadowSize: [30, 44],
-  iconAnchor: [12, 84],
+  iconAnchor: [22, 84],
   shadowAnchor: [4, 62],
   popupAnchor: [-3, -76],
 });
