60baa409aa0a693d3c0045330ddee9e5c87d6e8c
marynag <mgrabchak1109@gmail.com>
2023-07-20 11:21:17 +0300

    fix: iphone zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,10 @@ export default function RootLayout({
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1"
+        />
       </head>
       <body>
         <div
