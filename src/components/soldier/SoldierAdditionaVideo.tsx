8cfd7d5c411de2d077483792359cad91e090e136
marynag <mgrabchak1109@gmail.com>
2023-07-25 17:51:50 +0300

    fix: youtube link

--- src/components/soldier/SoldierAdditionaVideo.tsx
+++ src/components/soldier/SoldierAdditionaVideo.tsx
@@ -5,10 +5,22 @@ type ISoldierAdditionalVideoProps = {
   videoDescription: string;
 };
 
+function getId(url: string) {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+
+  return match && match[2].length === 11 ? match[2] : null;
+}
+
 export const SoldierAdditionalVideo = ({
   videoUrl,
   videoDescription,
 }: ISoldierAdditionalVideoProps) => {
+  //const videoId = videoUrl.split('youtu.be/')[1];
+  const videoId = getId(videoUrl);
+  const youtubeUrl = `https://www.youtube.com/embed/${videoId}`;
+
+  ('https://youtu.be/0-7IHOXkiV8?list=RDoHRNrgDIJfo');
   return (
     <div className="mt-3">
       <p className="text-sm text-grey-20">{videoDescription}</p>
@@ -16,7 +28,7 @@ export const SoldierAdditionalVideo = ({
         width="100%"
         height="224"
         className="rounded-lg bg-grey-30 mt-2 photo-shadow"
-        src={videoUrl}
+        src={youtubeUrl}
       ></iframe>
     </div>
   );
