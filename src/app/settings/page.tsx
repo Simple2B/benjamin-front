d770f62d16a259b3beb3f4d251426018ea50dd9b
marynag <mgrabchak1109@gmail.com>
2023-10-06 10:57:20 +0300

    fix setting cache

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { ApiService, CemeteriesService } from '@/openapi';
 import PreviewerSettings from '@/components/settings/PreviewerSettings';
 
-// export const dynamic = 'force-dynamic';
-export const fetchCache = 'default-cache';
 export const revalidate = 60;
 
 const Page = async () => {
